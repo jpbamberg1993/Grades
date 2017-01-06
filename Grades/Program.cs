d75e5d00428e61b68c06b0c30af77979cbcce8c5
John Paul Bamberg <jpbamberg1993@gmail.com>
2017-01-06 13:44:21 -0500

    Add letter grade and description

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -11,37 +11,26 @@ namespace Grades
         static void Main(string[] args)
         {
             GradeBook book = new GradeBook();
-
-            book.NameChanged += OnNameChanged;
             
-            book.Name = "John's Grade Book";
-            book.Name = "Grade Book";
-
-            book.Name = null;
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(book.Name);
             WriteResult("Average", stats.AverageGrade);
-            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Highest", stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
-        }
-        
-        static void OnNameChanged(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+            WriteResult(stats.Description, stats.LetterGrade);
         }
 
-        static void WriteResult(string description, int result)
+        static void WriteResult(string description, string result)
         {
             Console.WriteLine(description + ": " + result);
         }
 
         static void WriteResult(string description, float result)
         {
-            Console.WriteLine(description + ": " + result);
+            Console.WriteLine($"{description}: {result:F2}");
         }
     }
 }
